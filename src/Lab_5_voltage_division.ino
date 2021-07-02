2c4e2990edd2ff0f9ed92e424dccdf54a008540f
AKK2112 <aleckk18@gmail.com>
2021-07-01 23:45:34 -0500

    comments

--- src/Lab_5_voltage_division.ino
+++ src/Lab_5_voltage_division.ino
@@ -10,13 +10,15 @@ void setup() {
 }
 
 void loop() {
-  // read the value from the sensor
+  // read the value from the light sensor
   sensorValue = analogRead(A5);
-  // print the sensor reading so you know its range
+  // use the serial monitor to identify the max and min range
   Serial.println(sensorValue);
-   // map the sensor reading to a range for the LED
+   // turn the led on to a brightness specified from the map function.
   max(3485, sensorValue);
   min(0, sensorValue);
   analogWrite(D8, map(sensorValue, 0, 1023, 0, 255));
-
+//I had delay in before to find the max and min ranges in the serial monitor, 
+//but I removed it because it would delay the led and looked weird.
+//No delay significantly decreases the delay in the change of brightness and looks much smoother overall.
 }
